0aa3f35432dcf75b0f07d74177df4bd6c5d5c045
Yonier Vasquez Marin <yonier.marinm@opitech.com.co>
2022-11-10 11:08:33 -0500

    tabla finalizada

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 import { IceCreamService } from "./services/ice-cream.service.js";
 
+// Obtener los helados
 const iceCreamService = new IceCreamService();
-const iceCreams = iceCreamService.getIceCreams();
+
+
+const iceCreamReportApp = angular.module("iceCreamReportApp", []);
+
+iceCreamReportApp.controller("iceCreamReportController", function($scope) {
+    const iceCreamsList = iceCreamService.getIceCreams();
+    $scope.iceCreamsList = iceCreamsList;
+    $scope.updateTable = function() {
+        $scope.iceCreamsList = iceCreamService.getIceCreams();
+    }
+});
