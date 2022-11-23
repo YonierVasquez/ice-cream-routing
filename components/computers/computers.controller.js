a32936be73c0d2ea077d0f601e6f80a8ed828f5c
Yonier Vasquez Marin <yonier.marinm@opitech.com.co>
2022-11-22 23:11:02 -0500

    servicio de computadores creado

--- components/computers/computers.controller.js
+++ components/computers/computers.controller.js
@@ -1,6 +1,12 @@
-(function(angular) {
+(function (angular) {
 
-    angular.module('computersModule', [])
+    angular.module('computersModule', ['computersService'])
+
+        .controller('computersController', ['$scope', 'computersService', function ($scope, computersService) {
+            computersService.getComputers().then(function (computers) {
+                $scope.computersList = computers
+            })
+        }])
 
         .component('computersComponent', {
             templateUrl: 'components/computers/computers.view.html',
---
+++ services/computers.service.js
@@ -0,0 +1,147 @@
+(function(angular) {
+
+    // Datos de pruebas
+    let computersList = [
+        {
+            "id": 1,
+            "marca": "Lenovo",
+            "modelo": "ThinkPad T480",
+            "identification": "serial-1234abM",
+            "yearLanzamiento": "2018",
+            "color": "Azul metálico",
+            "capacidadDefectoRam": 4,
+            "capacidadMaximaRam": 32,
+            "procesador": "Intel Core i7-8550U",
+            "tipoDisco": "SSD",
+            "capacidadDisco": 512,
+            "tipoComputador": "Portátil"
+        },
+        {
+            "id": 2,
+            "marca": "Lenovo",
+            "modelo": "ThinkPad T480",
+            "identification": "serial-1234abM",
+            "yearLanzamiento": "2018",
+            "color": "Azul metálico",
+            "capacidadDefectoRam": 4,
+            "capacidadMaximaRam": 32,
+            "procesador": "Intel Core i7-8550U",
+            "tipoDisco": "SSD",
+            "capacidadDisco": 512,
+            "tipoComputador": "Portátil"
+        },
+        {
+            "id": 3,
+            "marca": "Lenovo",
+            "modelo": "ThinkPad T480",
+            "identification": "serial-1234abM",
+            "yearLanzamiento": "2018",
+            "color": "Azul metálico",
+            "capacidadDefectoRam": 4,
+            "capacidadMaximaRam": 32,
+            "procesador": "Intel Core i7-8550U",
+            "tipoDisco": "SSD",
+            "capacidadDisco": 512,
+            "tipoComputador": "Portátil"
+        },
+        {
+            "id": 4,
+            "marca": "Lenovo",
+            "modelo": "ThinkPad T480",
+            "identification": "serial-1234abM",
+            "yearLanzamiento": "2018",
+            "color": "Azul metálico",
+            "capacidadDefectoRam": 4,
+            "capacidadMaximaRam": 32,
+            "procesador": "Intel Core i7-8550U",
+            "tipoDisco": "SSD",
+            "capacidadDisco": 512,
+            "tipoComputador": "Portátil"
+        },
+        {
+            "id": 5,
+            "marca": "Lenovo",
+            "modelo": "ThinkPad T480",
+            "identification": "serial-1234abM",
+            "yearLanzamiento": "2018",
+            "color": "Azul metálico",
+            "capacidadDefectoRam": 4,
+            "capacidadMaximaRam": 32,
+            "procesador": "Intel Core i7-8550U",
+            "tipoDisco": "SSD",
+            "capacidadDisco": 512,
+            "tipoComputador": "Portátil"
+        },
+        {
+            "id": 6,
+            "marca": "Lenovo",
+            "modelo": "ThinkPad T480",
+            "identification": "serial-1234abM",
+            "yearLanzamiento": "2018",
+            "color": "Azul metálico",
+            "capacidadDefectoRam": 4,
+            "capacidadMaximaRam": 32,
+            "procesador": "Intel Core i7-8550U",
+            "tipoDisco": "SSD",
+            "capacidadDisco": 512,
+            "tipoComputador": "Portátil"
+        },
+        {
+            "id": 7,
+            "marca": "Lenovo",
+            "modelo": "ThinkPad T480",
+            "identification": "serial-1234abM",
+            "yearLanzamiento": "2018",
+            "color": "Azul metálico",
+            "capacidadDefectoRam": 4,
+            "capacidadMaximaRam": 32,
+            "procesador": "Intel Core i7-8550U",
+            "tipoDisco": "SSD",
+            "capacidadDisco": 512,
+            "tipoComputador": "Portátil"
+        },
+        {
+            "id": 8,
+            "marca": "Lenovo",
+            "modelo": "ThinkPad T480",
+            "identification": "serial-1234abM",
+            "yearLanzamiento": "2018",
+            "color": "Azul metálico",
+            "capacidadDefectoRam": 4,
+            "capacidadMaximaRam": 32,
+            "procesador": "Intel Core i7-8550U",
+            "tipoDisco": "SSD",
+            "capacidadDisco": 512,
+            "tipoComputador": "Portátil"
+        },
+        {
+            "id": 9,
+            "marca": "Lenovo",
+            "modelo": "ThinkPad T480",
+            "identification": "serial-1234abM",
+            "yearLanzamiento": "2018",
+            "color": "Azul metálico",
+            "capacidadDefectoRam": 4,
+            "capacidadMaximaRam": 32,
+            "procesador": "Intel Core i7-8550U",
+            "tipoDisco": "SSD",
+            "capacidadDisco": 512,
+            "tipoComputador": "Portátil"
+        }
+    ]
+
+
+
+
+
+    angular.module("computersService", [])
+
+        .factory("computersService", function () {
+            return {
+                getComputers: function () {
+                    return Promise.resolve(computersList)
+                }
+            }
+        })
+
+})(window.angular)
